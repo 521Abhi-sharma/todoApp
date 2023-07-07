2ac029599a5dbd6090f6b44f0943c974642f20ae
abhishek sharma <abhisheksharma.091221@gmail.com>
2023-07-07 19:40:53 +0530

    remove commneted code

--- src/Component/TodoForm.js
+++ src/Component/TodoForm.js
@@ -112,7 +112,7 @@ function TodoForm() {
                 <div className="list_action_container">
                     <div className="search_list">
                         <Form.Control className="filterListByDate" type="date"  value={searchByDate} onChange={searchByDateHandler} /> 
-                        <p>filter by date</p>
+                        {/* <p>filter by date</p> */}
                     </div>
                     <div className="show_all_list" onClick={getAllListHandler}>
                         <VscIssueReopened/>
